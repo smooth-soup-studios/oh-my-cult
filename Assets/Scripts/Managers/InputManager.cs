2813eab3e5315afb04d86a9238d528bef3760cfc
Milano <36030923+SyncMQ@users.noreply.github.com>
2024-03-01 12:43:39 +0100

    Merge pull request #17
    * + Added the control scheme for keyboard & mouse + any controller. You…
    
    * + Created an InputManager class to keep all the input centralized;
    
    * ~ Changed the naming for the bindings.

---
+++ Assets/Scripts/Managers/InputManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace Managers {
+public class InputManager : MonoBehaviour {
+	public static InputManager Instance { get; set; }
+	Controls Controls { get; set; }
+
+	void Awake() {
+		if (Instance != null && Instance != this) {
+			Controls?.Disable();
+			Destroy(gameObject);
+			return;
+		}
+
+		Instance = this;
+		DontDestroyOnLoad(gameObject);
+		Controls = new Controls();
+	}
+
+	void OnEnable() {
+		Controls?.Enable();
+	}
+
+	void OnDisable() {
+		Controls?.Disable();
+	}
+}
+}

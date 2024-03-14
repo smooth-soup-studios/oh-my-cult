fc92f80fdcb8cc3ede40f788c12fa121d7412296
Justwouter <wouter2312@gmail.com>
2024-03-14 12:51:09 +0100

    Added some nice conditional compiling to avoid befouling the logs

--- Assets/Scripts/Libraries/Logger.cs
+++ Assets/Scripts/Libraries/Logger.cs
@@ -2,14 +2,26 @@ using UnityEngine;
 
 public static class Logger {
     public static void Log(string name, string message) {
-        Debug.Log($"<color=silver>[  <color=lime>{name}</color>  ]: " + message + "</color>");
+		#if UNITY_EDITOR
+			Debug.Log($"<color=silver>[  <color=lime>{name}</color>  ]: " + message + "</color>");
+		#else
+			Debug.Log($"[{name}]: {message}");
+		#endif
     }
 
     public static void LogWarning(string name, string message) {
-        Debug.Log($"<color=silver>[  <color=orange>{name}</color>  ]: " + message + "</color>");
+		#if UNITY_EDITOR
+			Debug.Log($"<color=silver>[  <color=orange>{name}</color>  ]: " + message + "</color>");
+		#else
+			Debug.Log($"[{name}]: {message}");
+		#endif
     }
 
     public static void LogError(string name, string message) {
-        Debug.Log($"<color=silver>[  <color=red>{name}</color>  ]: " + message + "</color>");
+		#if UNITY_EDITOR
+			Debug.Log($"<color=silver>[  <color=red>{name}</color>  ]: " + message + "</color>");
+		#else
+			Debug.Log($"[{name}]: {message}");
+		#endif
     }
 }

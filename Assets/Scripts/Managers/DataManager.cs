22c41eb398aa7de60b3064b51ca8bfe18a0173ff
Milano <36030923+SyncMQ@users.noreply.github.com>
2024-03-14 12:38:27 +0100

    Game Manager  (#22)
    * Finished base logic for movement:
    
    + Added Movement controller with a basic move method;
    + Added Move property to be used from InputManager;
    + Added Managers GmaeObject and attached MovementController to it
    + Added MovementController to the Placeholder PLayer GameObjects
    
    * + Added MessagePack to the project. Considered using BinaryFormatter, but didn't for insecure reasons. Please read https://learn.microsoft.com/en-us/dotnet/standard/serialization/binaryformatter-security-guide
    
    I will add more in detail in the documentation, but for the curious: https://github.com/MessagePack-CSharp/MessagePack-CSharp?tab=readme-ov-file#aot-code-generation-support-for-unityxamarin
    
    + Generated the Resolver and Formatter in MessagePackResolver. This is required to be able to run serialize and deserialize. If there is a new data class added, you need to regenerate the files in Window -> MessagePack -> CodeGenerator and point to the right folder for the resolver and the .csproj of the unity project;
    
    + Added a folder for all data that needs to be saved ( has an empty GameData class to be used for later; )
    + Added a save and load functionality. It will save to and load from the default persistent data path ( C:/Users/[user]/AppData/LocalLow/Smooth Soup Studios/oh-my-cult/game-data.bin; )
    
    * FormatterNotRegisteredException is a result of you adding a class annotated with [MessagePackObject]. To solve that you need to regenerate your files ( see above. )
    
    * Finished save feature.
    Also restructured the directory structure.
    Please review the current directory structure and commit.
    
    * ~ Replaced functionality within DoesSceneExist() upon review with a smaller return statement ( same logic, less code )

---
+++ Assets/Scripts/Managers/DataManager.cs
@@ -0,0 +1,45 @@
+﻿using System.IO;
+using Data;
+using MessagePack;
+using MessagePack.Resolvers;
+using UnityEngine;
+
+namespace Managers {
+public class DataManager : MonoBehaviour {
+	public static DataManager Instance;
+	void Awake() {
+		if (Instance != null && Instance != this) {
+			Destroy(gameObject);
+			return;
+		}
+
+		Instance = this;
+		DontDestroyOnLoad(gameObject);
+		InitMessagePackResolver();
+	}
+
+	void InitMessagePackResolver() {
+		MessagePackSerializer.DefaultOptions = MessagePackSerializer.DefaultOptions.WithResolver(GeneratedResolver.Instance);
+	}
+
+	void SaveGame(GameData data) {
+		var bytes = MessagePackSerializer.Serialize(data);
+		var path = Application.persistentDataPath + "/game-data.bin";
+		File.WriteAllBytes(path, bytes);
+		Debug.Log($"Data saved to: {path}");
+	}
+
+	GameData LoadGame() {
+		var path = Application.persistentDataPath + "/game-data.bin";
+		if (!File.Exists(path)) {
+			Debug.LogError("Save file not found.");
+			return null;
+		}
+
+		var bytes = File.ReadAllBytes(path);
+		var loadedGameData = MessagePackSerializer.Deserialize<GameData>(bytes);
+		Debug.Log($"Data loaded from: {path}");
+		return loadedGameData;
+	}
+}
+}

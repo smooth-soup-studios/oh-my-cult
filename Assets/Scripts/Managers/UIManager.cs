b7a57e4aeddf8fc481f41fa0e095b14c8c7b37a0
Qy <HIHIQY1@gmail.com>
2024-03-19 10:56:51 +0100

    Create UIManager.cs

---
+++ Assets/Scripts/Managers/UIManager.cs
@@ -0,0 +1,19 @@
+namespace Managers
+{
+    public class UIManager : MonoBehaviour
+    {
+        public static UIManager Instance { get; private set; }
+
+        private void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}

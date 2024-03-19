d3627319931805e7c5b602d2fb5fac504a7bc6c9
Qy <HIHIQY1@gmail.com>
2024-03-19 16:30:42 +0100

    Animated dialog box, once more
    + Debug scene with toggle
    + main_menu.uxml
    + style.uss (what da hell :skull:)

--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -1,16 +1,37 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
 namespace Managers {
     public class UIManager : MonoBehaviour {
         public static UIManager Instance { get; private set; }
 
+        private VisualElement _root;
+
         private void Awake() {
             if (Instance == null) {
                 Instance = this;
+                _root = GetComponent<UIDocument>().rootVisualElement;
             }
             else {
                 Destroy(gameObject);
             }
+        }
+
+        public void ToggleDialogBox() {
+            if (_root.Q<VisualElement>("dialog-box").ClassListContains("closed")) {
+                ShowDialogBox();
+            }
+            else {
+                HideDialogBox();
+            }
+        }
+
+        public void ShowDialogBox() {
+            _root.Q<VisualElement>("dialog-box").RemoveFromClassList("closed");
+        }
+
+        public void HideDialogBox() {
+            _root.Q<VisualElement>("dialog-box").AddToClassList("closed");
         }
     }
 }

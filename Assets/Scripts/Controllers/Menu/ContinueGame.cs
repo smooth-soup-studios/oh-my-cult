6b391fe80aaa1fa5d87dd904c9a82b4af4300d88
TimothyTichelaar <21108366@student.hhs.nl>
2024-03-19 11:54:01 +0100

    ContinueGame setup zonder functionaliteit want we hebben nog geen save

---
+++ Assets/Scripts/Controllers/Menu/ContinueGame.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using Controllers.Menu;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ContinueGame : Menuitem
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // This method will define the action on mous click
+	public override void OnMouseUp() {
+        //Overleggen met Wouter hoe we een gamesave inladen
+		throw new System.NotImplementedException();
+	}
+}

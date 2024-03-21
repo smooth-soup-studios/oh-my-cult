081bcd31d257939498e721599d35f0b76270e68f
TimothyTichelaar <21108366@student.hhs.nl>
2024-03-21 12:05:02 +0100

    Made the methods public so they could be called from the textmesh buttons and the quit method logs being pressed in unity

--- Assets/Scripts/Controllers/Menu/MenuController.cs
+++ Assets/Scripts/Controllers/Menu/MenuController.cs
@@ -1,25 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace Controllers.Menu{
-    public class MenuController //: MonoBehaviour
+    public class MenuController : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
-        {
+    //     // Start is called before the first frame update
+    //     void Start()
+    //     {
             
-        }
+    //     }
 
-        // Update is called once per frame
-        void Update()
-        {
+    //     // Update is called once per frame
+    //     void Update()
+    //     {
             
-        }
+    //     }
 
-        //Start a game withoout a save file
-        void startNewGame(){
+        //Start a game without a save file
+        public void startNewGame(){
             //SceneManager will load the specified scene to start the gameplay
+            // can be made to load the next scene instead of a fixed one
             SceneManager.LoadScene(1);
             //throw new System.NotImplementedException();
         }
@@ -31,13 +33,16 @@ namespace Controllers.Menu{
         }
 
         //Quit the application
-        void quitToDesktop(){
+        public void quitToDesktop(){
+            //Debug print to show it works within unity
+            Debug.Log("Quit!");
+            // This wont work in unity itself but it does with a build
             Application.Quit();
         }
 
         //Navigate back to the main menu
-        void quitToMainMenu(){
-            //return to the scene where the main menu 
+        public void quitToMainMenu(){
+            //return to the scene whith the main menu 
             SceneManager.LoadScene(0);
         }
     }

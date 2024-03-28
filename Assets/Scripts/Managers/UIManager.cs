ae0fdd18cc596aeddb0d34e1e7132fa512efff6d
Justwouter <wouter2312@gmail.com>
2024-03-28 12:26:11 +0100

    Moved some values from the statemachine to the UIManager to make it more self contained during testing

--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -2,57 +2,55 @@ using UnityEngine;
 using UnityEngine.UIElements;
 
 namespace Managers {
-    public class UIManager : MonoBehaviour {
-        public static UIManager Instance { get; private set; }
+	public class UIManager : MonoBehaviour {
+		public static UIManager Instance { get; private set; }
 
-        public StateMachine PlayerStateMachine;
+		[SerializeField] private bool _hasPlaytestKey = false;
+		[SerializeField] private float _debugHealth = .5f;
 
-        private VisualElement _root;
-        private VisualElement _healthBarValue;
-        private VisualElement _keyIndicator;
 
-        private float _debugHealth = .5f;
+		private VisualElement _root;
+		private VisualElement _healthBarValue;
+		private VisualElement _keyIndicator;
 
-        private void Awake() {
-            if (Instance == null) {
-                if (PlayerStateMachine == null) {
-                    throw new System.Exception("PlayerStateMachine is not set in UIManager");
-                }
 
-                Instance = this;
-                _root = GetComponent<UIDocument>().rootVisualElement;
-                _healthBarValue = _root.Q<VisualElement>("health-bar-value");
-                _keyIndicator = _root.Q<VisualElement>("key-indicator");
-            }
-            else {
-                Destroy(gameObject);
-            }
-        }
+		private void Awake() {
+			if (Instance == null) {
 
-        private void Update() {
-            _healthBarValue.style.width = new StyleLength(new Length(PlayerStateMachine.Health * 100, LengthUnit.Percent));
-            _keyIndicator.style.visibility = PlayerStateMachine.HasPlaytestKey ? Visibility.Visible : Visibility.Hidden;
-        }
+				Instance = this;
+				_root = GetComponent<UIDocument>().rootVisualElement;
+				_healthBarValue = _root.Q<VisualElement>("health-bar-value");
+				_keyIndicator = _root.Q<VisualElement>("key-indicator");
+			}
+			else {
+				Destroy(gameObject);
+			}
+		}
 
-        public void ToggleDialogBox() {
-            if (_root.Q<VisualElement>("dialog-box").ClassListContains("closed")) {
-                ShowDialogBox();
-            }
-            else {
-                HideDialogBox();
-            }
-        }
+		private void Update() {
+			_healthBarValue.style.width = new StyleLength(new Length(_debugHealth * 100, LengthUnit.Percent));
+			_keyIndicator.style.visibility = _hasPlaytestKey ? Visibility.Visible : Visibility.Hidden;
+		}
 
-        public void ShowDialogBox() {
-            _root.Q<VisualElement>("dialog-box").RemoveFromClassList("closed");
-        }
+		public void ToggleDialogBox() {
+			if (_root.Q<VisualElement>("dialog-box").ClassListContains("closed")) {
+				ShowDialogBox();
+			}
+			else {
+				HideDialogBox();
+			}
+		}
 
-        public void HideDialogBox() {
-            _root.Q<VisualElement>("dialog-box").AddToClassList("closed");
-        }
+		public void ShowDialogBox() {
+			_root.Q<VisualElement>("dialog-box").RemoveFromClassList("closed");
+		}
 
-        public void DebugCycleHealth() {
-            _debugHealth = 1 - ((1 - _debugHealth + .1f) % 1);
-        }
-    }
+		public void HideDialogBox() {
+			_root.Q<VisualElement>("dialog-box").AddToClassList("closed");
+		}
+
+		public void DebugCycleHealth() {
+			_debugHealth = 1 - ((1 - _debugHealth + .1f) % 1);
+		}
+	}
 }

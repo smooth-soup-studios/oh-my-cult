8ab9c56546857404f0c4ad8dcb71a71d86a95c16
TimothyTichelaar <21108366@student.hhs.nl>
2024-03-28 11:17:24 +0100

    Made the Next button work to start the game

--- Assets/Scripts/Controllers/Menu/TempTutorial.cs
+++ Assets/Scripts/Controllers/Menu/TempTutorial.cs
@@ -5,7 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class TempTutorial : MonoBehaviour
 {
-    void StartGame(){
+    public void StartGame(){
+        //Set the actual game scene in the third slot for this to work
         SceneManager.LoadSceneAsync(2);
     }
     

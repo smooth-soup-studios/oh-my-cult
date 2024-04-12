9596f356464043199c473e9605f4bacb379a6e04
TimothyTichelaar <21108366@student.hhs.nl>
2024-04-12 14:37:26 +0200

    intro and outro with different tekst according to win or death

---
+++ Assets/Scripts/Controllers/Menu/PLaytest/PLaytestForm.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+
+public class PLaytestForm : MonoBehaviour
+{
+    Button _yesButton;
+    Button _noButton;
+    VisualElement _root;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _root = GetComponent<UIDocument>().rootVisualElement;
+        _yesButton = _root.Q<Button>("YesButton");
+        _yesButton.clicked += OnYesButton;
+        _noButton = _root.Q<Button>("NoButton");
+        _noButton.clicked += OnNoButton;
+    }
+
+    void OnYesButton(){
+        Logger.Log("Forms", "Opening the form");
+        Application.OpenURL("http://unity3d.com/");
+    }
+
+    void OnNoButton(){
+        SceneManager.LoadSceneAsync(0);
+    }
+}

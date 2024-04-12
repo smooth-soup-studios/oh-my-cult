07d0e2bd96b946b0447671b86f987a90269d7858
Milano <milanohoogvliets@hotmail.com>
2024-04-12 10:10:16 +0200

    basic sound stuff, added sounds and sound track to be used. also quickly added that sprite atlas. Added the mixer for sound ( master, sfx and music )

---
+++ Assets/Scripts/Managers/SoundManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace Managers {
+public class SoundManager : MonoBehaviour {
+	public static SoundManager Instance;
+	[SerializeField] AudioSource soundObject;
+	void Awake() {
+		if (Instance != null && Instance != this) {
+			Destroy(gameObject);
+			return;
+		}
+
+		Instance = this;
+		DontDestroyOnLoad(gameObject);
+	}
+
+	public void PlayClip(AudioClip audioClip, Transform spawnTransform, float volume) {
+		AudioSource audioSource = Instantiate(soundObject, spawnTransform.position, Quaternion.identity);
+		audioSource.clip = audioClip;
+		audioSource.volume = volume;
+		audioSource.Play();
+		float clipLength = audioSource.clip.length;
+
+		Destroy(audioSource, clipLength);
+	}
+
+}
+}

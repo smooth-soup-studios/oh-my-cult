832c11e9e52dc4923087a946137975bbbaef1d11
Qy <HIHIQY1@gmail.com>
2024-04-12 11:50:57 +0200

    Fixed GameManager
    * DoesSceneExist no longer crashes every time you uhhh
    * Made LoadCcene public

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -3,29 +3,37 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 namespace Managers {
-public class GameManager : MonoBehaviour {
-	public static GameManager Instance { get; private set; }
+	public class GameManager : MonoBehaviour {
+		public static GameManager Instance { get; private set; }
 
-	void Awake() {
-		if (Instance != null && Instance != this) {
-			Destroy(gameObject);
-			return;
+		void Awake() {
+			if (Instance != null && Instance != this) {
+				Destroy(gameObject);
+				return;
+			}
+
+			Instance = this;
+			DontDestroyOnLoad(gameObject);
 		}
 
-		Instance = this;
-		DontDestroyOnLoad(gameObject);
-	}
+		public void LoadScene(string scene) {
+			if (DoesSceneExist(scene)) SceneManager.LoadScene(scene);
+		}
 
-	void LoadScene(string scene) {
-		if (DoesSceneExist(scene)) SceneManager.LoadScene(scene);
-	}
+		static bool DoesSceneExist(string name) {
+			if (string.IsNullOrEmpty(name)) {
+				return false;
+			}
 
-	static bool DoesSceneExist(string name){
-		if (string.IsNullOrEmpty(name)) {
+			if (SceneManager.GetSceneByName(name).IsValid()) {
+				return true;
+			}
+
+			if (!int.TryParse(name, out int sceneInt)) {
+				return SceneManager.GetSceneByBuildIndex(sceneInt).IsValid();
+			}
+
 			return false;
 		}
-
-		return SceneManager.GetSceneByName(name).IsValid() || SceneManager.GetSceneByBuildIndex(int.Parse(name)).IsValid();
 	}
-}
 }

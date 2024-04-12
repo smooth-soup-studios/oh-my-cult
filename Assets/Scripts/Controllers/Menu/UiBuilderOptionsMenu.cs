d728a74d89ffdb5ab88b59c18d1934e1f695d411
TimothyTichelaar <21108366@student.hhs.nl>
2024-04-12 10:15:36 +0200

    Working menu with keybindings menu and a woriking slider for the master volume

--- Assets/Scripts/Controllers/Menu/UiBuilderOptionsMenu.cs
+++ Assets/Scripts/Controllers/Menu/UiBuilderOptionsMenu.cs
@@ -3,14 +3,18 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UIElements;
+using UnityEngine.Audio;
 
 public class UiBuilderOptionsMenu : MonoBehaviour
 {
-    [SerializeField] private Button _keyBindingButton;
-    [SerializeField] private Button _backButton;
+    Button _keyBindingButton;
+    Button _backButton;
+    Slider _masterVolume;
+    [SerializeField] private AudioMixer _audioMixer;
     [SerializeField] private GameObject _mainMenuUI;
     [SerializeField] private GameObject _keyBindingUI;
     [SerializeField] private GameObject _optionsUI;
+    [SerializeField] private float _baseVolume = 80;
     VisualElement _root;
 
     // Start is called before the first frame update
@@ -19,9 +23,17 @@ public class UiBuilderOptionsMenu : MonoBehaviour
          _root = GetComponent<UIDocument>().rootVisualElement;
         _keyBindingButton = _root.Q<Button>("KeyBinding");
         _backButton = _root.Q<Button>("Back");
+        _masterVolume = _root.Q<Slider>("MasterVolume");
+
+        _masterVolume.value = _baseVolume;
+        OnMasterSound(_baseVolume);
 
         _keyBindingButton.clicked += OnKeyBinding;
         _backButton.clicked += OnBack;
+        _masterVolume.RegisterCallback<ChangeEvent<float>>((evt) =>
+        {
+            OnMasterSound(evt.newValue - 80);
+        });
     }
 
     void OnKeyBinding(){
@@ -34,6 +46,10 @@ public class UiBuilderOptionsMenu : MonoBehaviour
         Logger.Log("OptionsMenu", "Back to Menu");
         _optionsUI.SetActive(false);
         _mainMenuUI.SetActive(true);
+    }
+
+    void OnMasterSound(float volume) {
+        _audioMixer.SetFloat("masterVolume", volume);
     }
 
 }

5a8308a5ad2466a2667df5a9bcce4fe017c00d57
TimothyTichelaar <21108366@student.hhs.nl>
2024-04-13 15:40:45 +0200

    New sliders for music and sound in the options menu + playtest scenes in another folder

--- Assets/Scripts/Controllers/Menu/UiBuilderOptionsMenu.cs
+++ Assets/Scripts/Controllers/Menu/UiBuilderOptionsMenu.cs
@@ -10,11 +10,13 @@ public class UiBuilderOptionsMenu : MonoBehaviour
     Button _keyBindingButton;
     Button _backButton;
     Slider _masterVolume;
+    Slider _musicVolume;
+    Slider _fxVolume;
     [SerializeField] private AudioMixer _audioMixer;
     [SerializeField] private GameObject _mainMenuUI;
     [SerializeField] private GameObject _keyBindingUI;
     [SerializeField] private GameObject _optionsUI;
-    [SerializeField] private float _baseVolume = 80;
+    [SerializeField] private float _baseVolume = 0;
     VisualElement _root;
 
     // Start is called before the first frame update
@@ -24,15 +26,29 @@ public class UiBuilderOptionsMenu : MonoBehaviour
         _keyBindingButton = _root.Q<Button>("KeyBinding");
         _backButton = _root.Q<Button>("Back");
         _masterVolume = _root.Q<Slider>("MasterVolume");
+        _musicVolume = _root.Q<Slider>("MusicVolume");
+        _fxVolume = _root.Q<Slider>("FXVolume");
 
         _masterVolume.value = _baseVolume;
         OnMasterSound(_baseVolume);
+        _musicVolume.value = _baseVolume;
+        OnMusicSound(_baseVolume);
+        _fxVolume.value = _baseVolume;
+        OnFXSound(_baseVolume);
 
         _keyBindingButton.clicked += OnKeyBinding;
         _backButton.clicked += OnBack;
         _masterVolume.RegisterCallback<ChangeEvent<float>>((evt) =>
         {
-            OnMasterSound(evt.newValue - 80);
+            OnMasterSound(evt.newValue);
+        });
+        _musicVolume.RegisterCallback<ChangeEvent<float>>((evt) =>
+        {
+            OnMusicSound(evt.newValue);
+        });
+        _fxVolume.RegisterCallback<ChangeEvent<float>>((evt) =>
+        {
+            OnFXSound(evt.newValue);
         });
     }
 
@@ -50,6 +66,14 @@ public class UiBuilderOptionsMenu : MonoBehaviour
 
     void OnMasterSound(float volume) {
         _audioMixer.SetFloat("masterVolume", volume);
+    }
+
+    void OnMusicSound(float volume) {
+        _audioMixer.SetFloat("musicVolume", volume);
+    }
+
+    void OnFXSound(float volume) {
+        _audioMixer.SetFloat("soundFXVolume", volume);
     }
 
 }

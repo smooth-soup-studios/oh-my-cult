acea1b6d4479e418afd6f7a343b5c2f941b9dba6
Justwouter <wouter2312@gmail.com>
2024-04-19 12:55:26 +0200

    Changed a script name, reordered some top level scene objects

--- Assets/Scripts/Managers/SaveManager/SaveTester.cs
+++ Assets/Scripts/Managers/SaveManager/SaveTester.cs
@@ -1,9 +1,10 @@
 using Managers;
 using UnityEngine;
 
-public class Tester : MonoBehaviour {
+public class SaveTester : MonoBehaviour {
 	private void Update() {
 		if (Input.GetKeyDown(KeyCode.L)) {
+			SaveManager.Instance.NewGame();
 			SaveManager.Instance.SaveGame();
 		}
 	}

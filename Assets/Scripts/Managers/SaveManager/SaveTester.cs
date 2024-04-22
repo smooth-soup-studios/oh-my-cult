16d3f6c927a69de4c3086b61b5867ea5e1519ebb
Justwouter <wouter2312@gmail.com>
2024-04-22 12:18:56 +0200

    Added shortcuts for save actions, added conditional compilation for the savetester

--- Assets/Scripts/Managers/SaveManager/SaveTester.cs
+++ Assets/Scripts/Managers/SaveManager/SaveTester.cs
@@ -1,11 +1,12 @@
-using Managers;
 using UnityEngine;
 
 public class SaveTester : MonoBehaviour {
 	private void Update() {
+#if UNITY_EDITOR
 		if (Input.GetKeyDown(KeyCode.L)) {
 			SaveManager.Instance.NewGame();
 			SaveManager.Instance.SaveGame();
 		}
+#endif
 	}
 }

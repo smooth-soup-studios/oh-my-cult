6981fb1aa55a8602831bf964690081723ab4c537
Justwouter <wouter2312@gmail.com>
2024-05-10 12:48:40 +0200

    Removed unused stuff and format

--- Assets/Scripts/Controllers/Menu/PLaytest/Outro.cs
+++ Assets/Scripts/Controllers/Menu/PLaytest/Outro.cs
@@ -1,44 +1,39 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UIElements;
 
-public class Outro : MonoBehaviour, ISaveable
-{
-    Button _yesButton;
-    Button _noButton;
-    VisualElement _root;
-    [SerializeField] private GameObject _deadUI;
-    [SerializeField] private GameObject _formUI;
-    VisualElement _hud;
-    // Start is called before the first frame update
-    void Start()
-    {
-        _root = GetComponent<UIDocument>().rootVisualElement;
-        GameObject HUD = GameObject.Find("HUD");
+public class Outro : MonoBehaviour, ISaveable {
+	Button _yesButton;
+	Button _noButton;
+	VisualElement _root;
+	[SerializeField] private GameObject _deadUI;
+	[SerializeField] private GameObject _formUI;
+
+	void Start() {
+		_root = GetComponent<UIDocument>().rootVisualElement;
+		GameObject HUD = GameObject.Find("HUD");
 		if (HUD) {
 			HUD.GetComponent<UIDocument>().rootVisualElement.visible = false;
 		}
-        _yesButton = _root.Q<Button>("YesButton");
-        _yesButton.clicked += OnYesButton;
-        _noButton = _root.Q<Button>("NoButton");
-        _noButton.clicked += OnNoButton;
-    }
+		_yesButton = _root.Q<Button>("YesButton");
+		_yesButton.clicked += OnYesButton;
+		_noButton = _root.Q<Button>("NoButton");
+		_noButton.clicked += OnNoButton;
+	}
 
-    void OnYesButton(){
-        SceneManager.LoadSceneAsync(0);
-    }
-    void OnNoButton(){
-        _deadUI.SetActive(false);
-        _formUI.SetActive(true);
-    }
+	void OnYesButton() {
+		SceneManager.LoadSceneAsync(0);
+	}
+	void OnNoButton() {
+		_deadUI.SetActive(false);
+		_formUI.SetActive(true);
+	}
 
 	public void LoadData(GameData data) {
-        if(!data.PlayerData.BossDefeated){
-            _formUI.SetActive(false);
-            _deadUI.SetActive(true);
-        }
+		if (!data.PlayerData.BossDefeated) {
+			_formUI.SetActive(false);
+			_deadUI.SetActive(true);
+		}
 	}
 
 	public void SaveData(GameData data) {

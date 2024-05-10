e4dfab0ac2ed2c3a4a509f1a7410da94a95dbb38
Qy <HIHIQY1@gmail.com>
2024-05-11 00:53:25 +0200

    Add actions to FoodItem

--- Assets/Scripts/Controllers/Inventory/Items/FoodItem.cs
+++ Assets/Scripts/Controllers/Inventory/Items/FoodItem.cs
@@ -2,10 +2,11 @@ public class FoodItem : InteractableItem {
 	public FoodStats FoodStats;
 
 	public override void PrimaryAction(StateMachine machine) {
-
+		machine.GetComponent<HealthController>().AddHealth(FoodStats.FoodData.HealthAmount);
+		machine.PlayerInventory.RemoveItem(ItemData);
 	}
 
 	public override void SecondaryAction(StateMachine machine) {
-
+		PrimaryAction(machine);
 	}
 }

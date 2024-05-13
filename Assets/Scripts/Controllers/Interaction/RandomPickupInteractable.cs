c0bba0c14a84314ce914feeef62304a119e11929
Justwouter <wouter2312@gmail.com>
2024-05-13 10:04:43 +0200

    Removed RandomPickup from the game

--- Assets/Scripts/Controllers/Interaction/RandomPickupInteractable.cs
+++ Assets/Scripts/Controllers/Interaction/RandomPickupInteractable.cs
@@ -1,43 +1,43 @@
-using UnityEditor;
-using UnityEngine;
+// using UnityEditor;
+// using UnityEngine;
 
-[RequireComponent(typeof(SpriteRenderer))]
-public class RandomPickupInteractable : MonoBehaviour {
-	private readonly static string _logname = "RandomPickupInteractable";
+// [RequireComponent(typeof(SpriteRenderer))]
+// public class RandomPickupInteractable : MonoBehaviour {
+// 	private readonly static string _logname = "RandomPickupInteractable";
 
-	[SerializeField] private MonoScript _itemScript;
-	[SerializeField] private InventoryItem[] _items;
+// 	[SerializeField] private MonoScript _itemScript;
+// 	[SerializeField] private InventoryItem[] _items;
 
-	private void Awake() {
-		if (_items.Length == 0) {
-			Logger.LogError(_logname, "No items to spawn");
-			return;
-		}
+// 	private void Awake() {
+// 		if (_items.Length == 0) {
+// 			Logger.LogError(_logname, "No items to spawn");
+// 			return;
+// 		}
 
-		if (_itemScript == null) {
-			Logger.LogError(_logname, "No item script to spawn");
-			return;
-		}
+// 		if (_itemScript == null) {
+// 			Logger.LogError(_logname, "No item script to spawn");
+// 			return;
+// 		}
 
-		if (!_itemScript.GetClass().IsSubclassOf(typeof(BaseItemPickupInteractable))) {
-			Logger.LogError(_logname, "Item script is not a subclass of ItemPickupInteractable");
-			return;
-		}
+// 		if (!_itemScript.GetClass().IsSubclassOf(typeof(BaseItemPickupInteractable))) {
+// 			Logger.LogError(_logname, "Item script is not a subclass of ItemPickupInteractable");
+// 			return;
+// 		}
 
-		InventoryItem item = _items[Random.Range(0, _items.Length)];
+// 		InventoryItem item = _items[Random.Range(0, _items.Length)];
 
-		BaseItemPickupInteractable interactable = gameObject.AddComponent(_itemScript.GetClass()) as BaseItemPickupInteractable;
+// 		BaseItemPickupInteractable interactable = gameObject.AddComponent(_itemScript.GetClass()) as BaseItemPickupInteractable;
 
-		interactable.Item = item;
+// 		interactable.Item = item;
 
-		Destroy(this);
-	}
+// 		Destroy(this);
+// 	}
 
-	private void OnDrawGizmos() {
-		if (transform == null)
-			return;
-		// Rainbow gizmos!!! :D
-		Gizmos.color = Color.HSVToRGB((float)EditorApplication.timeSinceStartup % 1, 1, .5f);
-		Gizmos.DrawWireSphere(transform.position, 15);
-	}
-}
+// 	private void OnDrawGizmos() {
+// 		if (transform == null)
+// 			return;
+// 		// Rainbow gizmos!!! :D
+// 		Gizmos.color = Color.HSVToRGB((float)EditorApplication.timeSinceStartup % 1, 1, .5f);
+// 		Gizmos.DrawWireSphere(transform.position, 15);
+// 	}
+// }

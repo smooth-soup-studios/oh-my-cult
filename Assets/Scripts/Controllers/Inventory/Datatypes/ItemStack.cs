aa62336cda0a022436db960310db8f2bba35768a
Justwouter <wouter2312@gmail.com>
2024-05-17 10:52:00 +0200

    Implemented most of the Inventory methods in Inv2, Added a cosnmtructor for ItemStack, Removed an unused method from Dict

--- Assets/Scripts/Controllers/Inventory/Datatypes/ItemStack.cs
+++ Assets/Scripts/Controllers/Inventory/Datatypes/ItemStack.cs
@@ -4,4 +4,9 @@ using System;
 public struct ItemStack {
 	public InventoryItem Item;
 	public int Amount;
+
+	public ItemStack(InventoryItem item, int amount) {
+		Item = item;
+		Amount = amount;
+	}
 }
--- Assets/Scripts/Controllers/Inventory/Inv2.cs
+++ Assets/Scripts/Controllers/Inventory/Inv2.cs
@@ -1,9 +1,14 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Inv2 : MonoBehaviour {
 	private string _logname = "InventorySystem";
+	[Header("Settings")]
+	[SerializeField] private bool _allowItemOverwrite = false;
+
+
 	private List<ItemStack> _currentInventory = new();
 	private int _maxInventorySize = 5;
 	private int _selectedItemIndex = 0;
@@ -12,82 +17,148 @@ public class Inv2 : MonoBehaviour {
 	private void Awake() {
 		// Need to initialize the list with the max size. Should be overwritten by LoadData if we ever add expanding inventory space.
 		_currentInventory = new(new ItemStack[_maxInventorySize]);
-		string sJon = JsonUtility.ToJson(_currentInventory.ToSerializable(), true);
-		Debug.Log(sJon);
+		EventBus.Instance.Subscribe<int>(EventType.HOTBAR_SELECT, SelectSlot);
+		EventBus.Instance.Subscribe<int>(EventType.HOTBAR_SWITCH, e => {
+			if (e > 0) {
+				SelectNextSlot();
+			}
+			else if (e < 0) {
+				SelectPrevSlot();
+			}
+		});
 	}
 
 
 
-	// public InventoryItem AddItem(InventoryItem item) {
-	// 	if (_currentInventory.Contains(null) && item != null) {
-	// 		_currentInventory[_currentInventory.FindIndex(x => x == null)] = item;
-	// 		return null;
-	// 	}
-	// 	else {
-	// 		InventoryItem oldItem = _currentInventory[_selectedItemIndex];
-	// 		_currentInventory[_selectedItemIndex] = item;
-	// 		return oldItem;
-	// 	}
+	public InventoryItem AddItem(InventoryItem item) {
+		// Check if item already exists in inventory
+		if (_currentInventory.Any(e => e.Item == item)) {
+			ItemStack stack = _currentInventory.First(e => e.Item == item);
+			stack.Amount++;
+			return null;
+		}
+		// Check if there is an empty slot in the inventory
+		else if (_currentInventory.Any(e => e.Amount == 0) && item != null) {
+			_currentInventory[_currentInventory.FindIndex(x => x.Amount == 0)] = new ItemStack(item, 1);
+			return null;
+		}
+		//overwrite the item if enabled
+		else {
+			if (!_allowItemOverwrite) {
+				return item;
+			}
+			InventoryItem oldItem = _currentInventory[_selectedItemIndex].Item;
+			_currentInventory[_selectedItemIndex] = new ItemStack(item, 1);
+			return oldItem;
+		}
+	}
 
-	// }
+	public ItemStack AddItem(ItemStack stack) {
+		// Check if item already exists in inventory
+		if (_currentInventory.Any(e => e.Item == stack.Item)) {
+			ItemStack existingStack = _currentInventory.First(e => e.Item == stack.Item);
+			existingStack.Amount += stack.Amount;
+			return new ItemStack(null, 0);
+		}
+		// Check if there is an empty slot in the inventory
+		else if (_currentInventory.Any(e => e.Amount == 0) && stack.Item != null) {
+			_currentInventory[_currentInventory.FindIndex(x => x.Amount == 0)] = stack;
+			return new ItemStack(null, 0);
+		}
+		//overwrite the item if enabled
+		else {
+			if (!_allowItemOverwrite) {
+				return stack;
+			}
+			ItemStack oldStack = _currentInventory[_selectedItemIndex];
+			_currentInventory[_selectedItemIndex] = stack;
+			return oldStack;
+		}
+	}
 
-	// public void RemoveItem(InventoryItem item) {
-	// 	RemoveItemByIndex(_currentInventory.IndexOf(item));
-	// }
+	public void RemoveItem(InventoryItem item) {
+		RemoveItemByIndex(_currentInventory.FindIndex(e => e.Item == item));
+	}
+	public void RemoveItem(InventoryItem item, int amount) {
+		ItemStack stack = _currentInventory.First(e => e.Item == item);
+		stack.Amount -= amount;
+		if (stack.Amount <= 0) {
+			RemoveItemByIndex(_currentInventory.FindIndex(e => e.Item == item));
+		}
+	}
+	public void RemoveItem(ItemStack stack) {
+		ItemStack existingStack = _currentInventory.First(e => e.Item == stack.Item);
+		existingStack.Amount -= stack.Amount;
+		if (existingStack.Amount <= 0) {
+			RemoveItemByIndex(_currentInventory.FindIndex(e => e.Item == stack.Item));
+		}
+	}
 
-	// public void RemoveItemByIndex(int index) {
-	// 	if (index >= 0 && index < _maxInventorySize && _currentInventory.Count > index) {
-	// 		_currentInventory[index] = null;
-	// 	}
-	// }
+	public void RemoveItemByIndex(int index) {
+		if (index >= 0 && index < _maxInventorySize && _currentInventory.Count > index) {
+			_currentInventory[index] = new(null, 0);
+		}
+	}
 
-	// public void SelectNextSlot() {
-	// 	if (_selectedItemIndex + 1 >= _maxInventorySize) {
-	// 		_selectedItemIndex = 0;
-	// 	}
-	// 	else {
-	// 		_selectedItemIndex++;
-	// 	}
-	// }
+	public void SelectNextSlot() {
+		if (_selectedItemIndex + 1 >= _maxInventorySize) {
+			_selectedItemIndex = 0;
+		}
+		else {
+			_selectedItemIndex++;
+		}
+	}
 
-	// public void SelectPrevSlot() {
-	// 	if (_selectedItemIndex - 1 < 0) {
-	// 		_selectedItemIndex = _maxInventorySize - 1;
-	// 	}
-	// 	else {
-	// 		_selectedItemIndex--;
-	// 	}
-	// }
+	public void SelectPrevSlot() {
+		if (_selectedItemIndex - 1 < 0) {
+			_selectedItemIndex = _maxInventorySize - 1;
+		}
+		else {
+			_selectedItemIndex--;
+		}
+	}
 
-	// public void SelectSlot(int index) {
-	// 	if (index >= 0 && index < _maxInventorySize) {
-	// 		_selectedItemIndex = index;
-	// 	}
-	// }
+	public void SelectSlot(int index) {
+		if (index >= 0 && index < _maxInventorySize) {
+			_selectedItemIndex = index;
+		}
+	}
 
-	// public InventoryItem GetSelectedItem() {
-	// 	if (_currentInventory.Count > _selectedItemIndex)
-	// 		return _currentInventory[_selectedItemIndex];
-	// 	return null;
-	// }
+	public InventoryItem GetSelectedItem() {
+		if (_currentInventory.Count > _selectedItemIndex)
+			return _currentInventory[_selectedItemIndex].Item;
+		return null;
+	}
 
-	// public InventoryItem GetItemByIndex(int index) {
-	// 	if (index >= 0 && index < _maxInventorySize && _currentInventory.Count > index)
-	// 		return _currentInventory[index];
-	// 	return null;
-	// }
+	public ItemStack GetSelectedStack() {
+		if (_currentInventory.Count > _selectedItemIndex)
+			return _currentInventory[_selectedItemIndex];
+		return new ItemStack(null, 0);
+	}
 
-	// public int GetCurrentIndex() {
-	// 	return _selectedItemIndex;
-	// }
+	public InventoryItem GetItemByIndex(int index) {
+		if (index >= 0 && index < _maxInventorySize && _currentInventory.Count > index)
+			return _currentInventory[index].Item;
+		return null;
+	}
 
-	// public int GetInventoryMaxSize() {
-	// 	return _maxInventorySize;
-	// }
+	public ItemStack GetStackByIndex(int index) {
+		if (index >= 0 && index < _maxInventorySize && _currentInventory.Count > index)
+			return _currentInventory[index];
+		return new ItemStack(null, 0);
+	}
 
-	// public bool IsInventoryFull() {
-	// 	return !_currentInventory.Any(e => e == null);
-	// }
+	public int GetCurrentIndex() {
+		return _selectedItemIndex;
+	}
+
+	public int GetInventoryMaxSize() {
+		return _maxInventorySize;
+	}
+
+	public bool IsInventoryFull() {
+		return !_currentInventory.Any(e => e.Amount == 0);
+	}
 
 
 

21532ad95d23433a40d1fec945537d45d1370f6a
Justwouter <wouter2312@gmail.com>
2024-05-23 00:25:30 +0200

    Fixed nullcheck for waypoints

--- Assets/Scripts/Controllers/AI/Enemy/Nodes/CheckAgentHasWaypoints.cs
+++ Assets/Scripts/Controllers/AI/Enemy/Nodes/CheckAgentHasWaypoints.cs
@@ -9,7 +9,7 @@ public class CheckAgentHasWaypoints : Node {
 	public override NodeState Evaluate(EnemyBehaviourTree tree) {
 		// just make this a single tree already -_-
 		EnemyBT enemyBT = tree as EnemyBT;
-		if (enemyBT == null || enemyBT.Waypoints == null || enemyBT.Waypoints.Length < 2 || enemyBT.Waypoints.Contains(null))
+		if (enemyBT == null || enemyBT.Waypoints == null || enemyBT.Waypoints.Length < 2 || enemyBT.Waypoints.Any(e => e == null))
 			return NodeState.FAILURE;
 		return NodeState.SUCCESS;
 

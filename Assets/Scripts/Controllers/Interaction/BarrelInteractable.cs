7f209265f95049257789075d24018a03be06ecf2
Qy <HIHIQY1@gmail.com>
2024-05-27 12:56:33 +0200

    how about you drop th…

--- Assets/Scripts/Controllers/Interaction/BarrelInteractable.cs
+++ Assets/Scripts/Controllers/Interaction/BarrelInteractable.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 [RequireComponent(typeof(SpriteRenderer))]
 [RequireComponent(typeof(ShatterController))]
 public class BarrelInteractable : BaseInteractable {
-	public InventoryItem DropItem;
+	public GameObject DroppingItemPrefab;
+	public GameObject PickupPointInteractable;
+	public InventoryItem ItemToDrop;
+
 	private ShatterController _shatterController;
 
 	private void Awake() {
@@ -23,12 +26,19 @@ public class BarrelInteractable : BaseInteractable {
 		base.Interact(interactor);
 		_shatterController.Shatter(interactor);
 		GetComponent<SpriteRenderer>().enabled = false;
+
+		if (PickupPointInteractable != null && ItemToDrop != null && DroppingItemPrefab != null) {
+			GameObject dip = Instantiate(DroppingItemPrefab, transform.position, Quaternion.identity);
+			dip.GetComponent<DroppingItemController>().PickupPointInteractable = PickupPointInteractable;
+			dip.GetComponent<DroppingItemController>().ItemToDrop = ItemToDrop;
+		}
+
 		if (SingleUse) {
 			Destroy(gameObject, _shatterController.ShatterLifetime);
 		}
 	}
 
 	private void OnValidate() {
-		// SingleUse = true;
+		SingleUse = true;
 	}
 }

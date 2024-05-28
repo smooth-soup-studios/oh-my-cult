91d309203b29a3174c15028e02b65a0bffd7dcd7
Justwouter <wouter2312@gmail.com>
2024-05-28 12:16:34 +0200

    Converted variables to protected in SaveManager, added conversion stuff to Dict

--- Assets/Scripts/Data/Dict.cs
+++ Assets/Scripts/Data/Dict.cs
@@ -1,6 +1,6 @@
 using System;
 using System.Collections.Generic;
-
+using System.Linq;
 using UnityEngine;
 
 [Serializable]
@@ -8,6 +8,15 @@ public class Dict<Tkey, Tvalue> : Dictionary<Tkey, Tvalue>, ISerializationCallba
 
 	[SerializeField] private List<Tkey> _keys = new();
 	[SerializeField] private List<Tvalue> _values = new();
+
+	public Dict() { }
+	public Dict(List<Tkey> keys, List<Tvalue> values) {
+		for (int i = 0; i < keys.Count; i++) {
+			Add(keys[i], values[i]);
+		}
+	}
+
+	public Dict(Dictionary<Tkey, Tvalue> inputDict) : this(inputDict.Keys.ToList(), inputDict.Values.ToList()) { }
 
 	public void OnBeforeSerialize() {
 		_keys.Clear();
@@ -29,5 +38,15 @@ public class Dict<Tkey, Tvalue> : Dictionary<Tkey, Tvalue>, ISerializationCallba
 		for (int i = 0; i < _keys.Count; i++) {
 			Add(_keys[i], _values[i]);
 		}
+	}
+}
+
+public static class DictionaryExtentions {
+	public static Dict<T, T> ToSerializable<T>(this Dictionary<T, T> inputDict) {
+		return new Dict<T, T>(inputDict);
+	}
+
+	public static Dictionary<T, T> ToRegular<T>(this Dict<T, T> inputDict) {
+		return inputDict;
 	}
 }

ef643aa68f6b806c501dc997e41ca848d1c7b385
Justwouter <wouter2312@gmail.com>
2024-06-05 16:11:38 +0200

    Basic audio manager w/test sound Added some events for audio

--- Assets/Scripts/Libraries/Eventbus/EventType.cs
+++ Assets/Scripts/Libraries/Eventbus/EventType.cs
@@ -2,20 +2,31 @@
 ///
 /// </summary>
 public enum EventType {
+	// Player input events
 	MOVEMENT,
-	PAUSE,
 	DASH,
 	INTERACT,
 	USE_PRIMARY,
 	USE_SECONDARY,
 	HOTBAR_SELECT,
 	HOTBAR_SWITCH,
+	PAUSE,
+
+	// State Events
 	DEATH,
+	HIT,
+	INV_ADD,
+	INV_REMOVE,
+
+	// Audio Events
+	AUDIO_PLAY,
+	AUDIO_STOP,
+	AUDIO_STOP_ALL,
+
+	// Map events
 	PLAYER_ENTER_BUILDING_COVER,
 	PLAYER_EXIT_BUILDING_COVER,
 	ENTER_CHURCH,
 	ENTER_VILLAGE,
-	HIT,
-	INV_ADD,
-	INV_REMOVE
+
 }

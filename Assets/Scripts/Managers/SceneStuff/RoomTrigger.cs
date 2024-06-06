dabb13ab766fe3ab2d5bfd33d8fb18366c2b6cb3
Studeeric <21146632@student.hhs.nl>
2024-06-07 01:27:18 +0200

    Added more borders lol said the scorpion lmao

--- Assets/Scripts/Managers/SceneStuff/RoomTrigger.cs
+++ Assets/Scripts/Managers/SceneStuff/RoomTrigger.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class RoomTrigger : MonoBehaviour {
+public class RoomTrigger : MonoBehaviour, ISaveable {
+
+	[field: SerializeField, Header("Object information")] public string ObjectId { get; private set; }
 
 	[SerializeField] private List<GameObject> _borders;
 	[SerializeField] private List<GameObject> _enemies;
@@ -41,6 +43,26 @@ public class RoomTrigger : MonoBehaviour {
 			border.SetActive(false);
 			_isCleared = true;
 		}
-		EventBus.Instance.TriggerEvent(EventType.INTERACT_TOGGLE, true);
+		if (EventBus.Instance){
+			EventBus.Instance.TriggerEvent(EventType.INTERACT_TOGGLE, true);
+		}
+	}
+
+		protected void OnValidate() {
+		// Generates an unique ID based on the name & position of the gameobject.
+#if UNITY_EDITOR
+		ObjectId = $"{name}-{Vector3.SqrMagnitude(transform.position)}";
+		UnityEditor.EditorUtility.SetDirty(this);
+#endif
+	}
+
+	public virtual void LoadData(GameData data) {
+		if (data.SceneData.ArbitraryTriggers.ContainsKey($"{ObjectId}-isCleared")) {
+			data.SceneData.ArbitraryTriggers.TryGetValue($"{ObjectId}-isCleared", out _isCleared);
+		}
+	}
+
+	public virtual void SaveData(GameData data) {
+		data.SceneData.ArbitraryTriggers[$"{ObjectId}-isCleared"] = _isCleared;
 	}
 }

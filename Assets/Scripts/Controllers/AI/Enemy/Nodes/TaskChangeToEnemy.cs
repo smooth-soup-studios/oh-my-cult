20c70c7b250a5bcdd02ba2990367c040f3f8aff3
Justwouter <wouter2312@gmail.com>
2024-06-15 20:39:27 +0200

    Moved firecontroller, changed ChangeToEnemyNode to disable the rigidbody of the enemy while transforming

--- Assets/Scripts/Controllers/AI/Enemy/Nodes/TaskChangeToEnemy.cs
+++ Assets/Scripts/Controllers/AI/Enemy/Nodes/TaskChangeToEnemy.cs
@@ -2,28 +2,36 @@ using UnityEngine;
 using BehaviorTree;
 
 public class TaskChangeToEnemy : Node {
-	public float AnimationWaitTime = .2f;
-	float _copyAnimationDuration;
-	bool _didAnimationStart = false;
-	float _tAnimationStart;
-	bool _didInstantiateAnimationPrefab = false;
+	private Rigidbody2D _rb;
+	private float _animationWaitTime = .2f;
+	private float _copyAnimationDuration;
+	private bool _didAnimationStart = false;
+	private float _tAnimationStart;
+	private bool _didInstantiateAnimationPrefab = false;
 
 	public override NodeState Evaluate(BaseBehaviourTree tree) {
 		if (!_didAnimationStart) {
 			_didAnimationStart = true;
 			_tAnimationStart = Time.time;
+			if (tree.TryGetComponent<Rigidbody2D>(out _rb)) {
+				_rb.simulated = false;
+			}
 		}
 		else {
-			if (Time.time - _tAnimationStart > AnimationWaitTime && !_didInstantiateAnimationPrefab) {
+			if (Time.time - _tAnimationStart > _animationWaitTime && !_didInstantiateAnimationPrefab) {
 				_didInstantiateAnimationPrefab = true;
 				GameObject _animationContainerInstance = Object.Instantiate(tree.NPCTransformPrefab, tree.transform);
 				_copyAnimationDuration = _animationContainerInstance.GetComponent<EnemyTransformAnimationContainerController>().Duration;
 			}
-			if (Time.time - _tAnimationStart > _copyAnimationDuration / 2 + AnimationWaitTime) {
+			if (Time.time - _tAnimationStart > _copyAnimationDuration / 2 + _animationWaitTime) {
+				// Why. I hate this. (I know why but still hate it.)
 				tree.HalfwayTransitionAnimation = true;
 			}
-			if (Time.time - _tAnimationStart > _copyAnimationDuration + AnimationWaitTime) {
+			if (Time.time - _tAnimationStart > _copyAnimationDuration + _animationWaitTime) {
 				tree.ActorType = ActorType.MeleeEnemy;
+				if (_rb) {
+					_rb.simulated = true;
+				}
 			}
 		}
 

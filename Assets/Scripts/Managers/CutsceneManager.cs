99a8fa2aa8ada48b4bbdc13e12d413fd45b519a5
Studeeric <21146632@student.hhs.nl>
2024-06-16 18:50:41 +0200

    Simplified csm with an enum

--- Assets/Scripts/Managers/CutsceneManager.cs
+++ Assets/Scripts/Managers/CutsceneManager.cs
@@ -1,25 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class CutsceneManager : MonoBehaviour {
 	// Start is called before the first frame update
+	private enum CutScene {
+		Intro = 14, // Name is humanname and number is duration in seconds.
+		Church = 5,
+		Outro = 3
+	}
 
+	[SerializeField] CutScene _cutScene;
 
 	void Start() {
-		// StartCoroutine(OutroCutscene());
-		StartCoroutine(IntroCutscene());
+		StartCoroutine(PlayCutscene());
 	}
 
-	public IEnumerator IntroCutscene() {
-		Logger.Log("Play", "intro");
-		yield return new WaitForSeconds(14f);
+	public IEnumerator PlayCutscene(){
+		Logger.Log("Play", _cutScene.HumanName());
+		yield return new WaitForSeconds((float)_cutScene);
 		SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex + 1);
 	}
-	public IEnumerator OutroCutscene() {
-		Logger.Log("Play", "Outro");
-		yield return new WaitForSeconds(3f);
-		SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex + 1);
-	}
+
+	// public IEnumerator IntroCutscene() {
+	// 	Logger.Log("Play", "intro");
+	// 	yield return new WaitForSeconds(14f);
+	// 	SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex + 1);
+	// }
+	// public IEnumerator OutroCutscene() {
+	// 	Logger.Log("Play", "Outro");
+	// 	yield return new WaitForSeconds(3f);
+	// 	SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex + 1);
+	// }
 }

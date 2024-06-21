4088e083938c52914901f9d9c12ed5b7e1ab6511
Qy <HIHIQY1@gmail.com>
2024-06-21 12:18:45 +0200

    Fix fireController to actually do the fadein correctly

--- Assets/Scripts/Managers/SceneStuff/FireController.cs
+++ Assets/Scripts/Managers/SceneStuff/FireController.cs
@@ -1,23 +1,25 @@
 using UnityEngine;
 using UnityEngine.Rendering.Universal;
 
-public class FireController : MonoBehaviour
-{
+public class FireController : MonoBehaviour {
 	[SerializeField] private Light2D _light2D;
-	[Range(1.0f, 15.0f)]
+	[Range(0.0f, 15.0f)]
 	[SerializeField] private float _minIntensity;
 	[Range(1.0f, 15.0f)]
 	[SerializeField] private float _maxIntensity;
 	private float _random;
 
-    void Start()
-    {
-		_random = Random.Range(0f, 65535.0f);
-    }
+	private float _spawnTime = 0.0f;
 
-    void Update()
-    {
-		float noise = Mathf.PerlinNoise(_random, Time.time);
-		_light2D.intensity = Mathf.Lerp(_minIntensity, _maxIntensity, noise);
-    }
+	void Start() {
+		_random = Random.Range(0f, 65535.0f);
+		_spawnTime = Time.time;
+	}
+
+	void Update() {
+		// float noise = Mathf.PerlinNoise(_random, Time.time);
+		// _light2D.intensity = Mathf.Lerp(_minIntensity, _maxIntensity, noise);
+		// Temp fix
+		_light2D.intensity = Mathf.Lerp(_minIntensity, _maxIntensity, Mathf.Clamp01((Time.time - _spawnTime) / 1f));
+	}
 }
